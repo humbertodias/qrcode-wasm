4c358814f661abf330b5ae53a59372ecf6f3d022
Humberto Dias <hldtux@gmail.com>
2024-02-08 01:13:30 -0300

    cache

--- assets/sw.js
+++ assets/sw.js
@@ -1,22 +1,44 @@
-// sw.js
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('qrcode-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js'
-                // Add paths to other static assets as needed
-            ]);
-        })
-    );
+CACHE_NAME = "qrcode-cache";
+URL_CACHE = ["/", "/index.html", "/styles.css", "/script.js"];
+
+self.addEventListener("install", (event) => {
+  console.log("sw.js installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(URL_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cache) => {
+      Promise.all(
+        cache.filter((item) => {
+          if (item != CACHE_NAME) return caches.delete(item);
         })
-    );
+      );
+    })
+  );
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        console.log("Already cached", res);
+        return res;
+      }
+      return fetch(event.request, { cache: "no-store" }).then(
+        (res) => {
+          console.log("Not cache", res);
+          return res;
+        },
+        (err) => {
+          return err;
+        }
+      );
+    })
+  );
 });
