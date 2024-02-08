652ae29844fddb4281104bd5e08200b534efb47d
Humberto Dias <hldtux@gmail.com>
2024-02-08 01:29:48 -0300

    dynamic port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	err := http.ListenAndServe(":8080", http.FileServer(http.Dir("./assets")))
+	port := flag.Int("p", 8080, "Port number")
+	flag.Parse()
+	fmt.Printf("Started on http://localhost:%d\n", *port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), http.FileServer(http.Dir("./assets")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
