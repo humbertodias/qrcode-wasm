7632276267c875b705b772fd491b4be69aca7488
Humberto Dias <hldtux@gmail.com>
2024-02-07 22:20:13 -0300

    size + favicon

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"syscall/js"
 
 	"bytes"
@@ -10,7 +11,7 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 )
 
-func generateQRCode(input string) (string, error) {
+func generateQRCode(size int, input string) (string, error) {
 	// Generate QR code for the input string
 	qr, err := qrcode.New(input, qrcode.Medium)
 	if err != nil {
@@ -19,7 +20,7 @@ func generateQRCode(input string) (string, error) {
 
 	// Encode the QR code as PNG to a buffer
 	var buf bytes.Buffer
-	if err := qr.Write(256, &buf); err != nil {
+	if err := qr.Write(size, &buf); err != nil {
 		return "", err
 	}
 
@@ -32,13 +33,20 @@ func generateQRCode(input string) (string, error) {
 
 func qrCodeB64Wrapper() js.Func {
 	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) != 1 {
+		if len(args) != 2 {
 			return "Invalid no of arguments passed"
 		}
-		inputText := args[0].String()
-		b64, err := generateQRCode(inputText)
+		sizeText := args[0].String()
+		size, err := strconv.Atoi(sizeText)
 		if err != nil {
-			fmt.Printf("unable to convert to json %s\n", err)
+			fmt.Printf("unable to convert to size %s\n", err)
+			return err.Error()
+		}
+
+		inputText := args[1].String()
+		b64, err := generateQRCode(size, inputText)
+		if err != nil {
+			fmt.Printf("unable to convert to qrcode %s\n", err)
 			return err.Error()
 		}
 		return b64
